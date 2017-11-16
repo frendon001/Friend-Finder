4b6adfcbe6174ba5700f693fba565b753daf0968
Fausto Rendon <faustor001@gmail.com>
2017-11-15 19:55:37 -0800

    Initial file updates

---
+++ FriendFinder/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+// ===============================================================================
+// LOAD DATA
+// ===============================================================================
+
+var friendsData = require("../data/friends.js");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+
+  // API POST Requests
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+
+      friendsData.push(req.body);
+
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+// ==============================================================================
+// NPM Dependancies
+// ==============================================================================
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// ==============================================================================
+// EXPRESS Setup
+// ==============================================================================
+
+// Creating an "express" server
+var app = express();
+
+// Sets Port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the parsing for Express
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTING
+// Point to route FIles
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
